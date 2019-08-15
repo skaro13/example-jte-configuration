60fa9ccd84f99a7a5c639bd3a3ca11112d129a2b
Simon Caron <simon.caron.8@gmail.com>
2019-08-15 15:11:58 -0400

    Update pipeline_config.groovy

--- pipeline_config.groovy
+++ pipeline_config.groovy
@@ -20,5 +20,14 @@ stages{
 steps{
     unit_test{
         stage = "Unit Test"
+        image = "maven"
+        command = "mvn clean verify"
+        stash{
+            name = "test-results"
+            includes = "./target"
+            excludes = "./src"
+            useDefaultExcludes = false
+            allowEmpty = true
+        }
     }
 }
